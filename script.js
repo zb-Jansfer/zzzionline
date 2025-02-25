69cd6a9bda007cae8275103980bb023e34a3f9e3
zb-Jansfer <zebinjsf@gmail.com>
2025-02-24 21:03:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         secretButton.addEventListener("click", function () {
             let answer = prompt("英雄");
             if (answer === "所见略同") {
-                secretMessageBox.style.display = "flex";
+                secretMessageBox.style.display = "flex"; secretMessageBox.style.flexDirection = "column"; secretMessageBox.style.justifyContent = "center"; secretMessageBox.style.alignItems = "center";
             } else {
                 alert("❌ 答案错误，请重新输入！");
             }
